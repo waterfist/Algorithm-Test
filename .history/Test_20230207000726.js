6eb137e3ef317edfc213ca3c9dc9f208363dfb65
hotsix <codinggo@naver.com>
2023-02-07 00:18:37 +0900

    success: Javascript 두 수의 차

--- .history/Test_20230207000726.js
+++ .history/Test_20230207000726.js
@@ -5,5 +5,4 @@ function mun(a, b) {
   let a = 1;
 
   return a - b;
-  console.log(hello world);
 }
--- .history/Test_20230207000813.js
+++ .history/Test_20230207000813.js
@@ -5,5 +5,5 @@ function mun(a, b) {
   let a = 1;
 
   return a - b;
-  console.log(hello world);
+  console.log();
 }
